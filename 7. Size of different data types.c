1096efc428437fa9f0b23b2fb284e6185ba7d6d8
Mohammad Rahil Khan <83690128+rahil8058@users.noreply.github.com>
2021-05-16 14:30:28 +0530

    Create 7. Size of different data types.c

---
+++ 7. Size of different data types.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    short e;
+    long f;
+    long double g;
+    printf("Size of int is %d bytes\n",sizeof(a));
+    printf("Size of float is %d bytes\n",sizeof(b));
+    printf("Size of char is %d bytes\n",sizeof(c));
+    printf("Size of double is %d bytes\n",sizeof(d));
+    printf("Size of short is %d bytes\n",sizeof(e));
+    printf("Size of long is %d bytes\n",sizeof(f));
+    printf("Size of long diubl is %d bytes\n",sizeof(g));
+    return 0;
+}
