0bf9ecc9c3118cca6d45e6f9ff4aaf309144c02d
Mohammad Rahil Khan <83690128+rahil8058@users.noreply.github.com>
2021-05-16 13:49:18 +0530

    Create 5. Add two integers.c

---
+++ 5. Add two integers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int var1;
+    int var2;
+    int var3;
+    var1=20;
+    var2=40;
+    var3=var1+var2;
+    printf("sum of %d and %d is %d",var1,var2,var3);
+    return 0;
+}
